862d143a0aebb3393e51b84b1191a77e6dd5a9eb
SamJDriver <SamJDriv@gmail.com>
2024-09-30 17:18:29 -0700

    Added forest floor

--- src/components/ForestFloor/ForestFloor.tsx
+++ src/components/ForestFloor/ForestFloor.tsx
@@ -5,29 +5,33 @@ import { WindowHeight, WindowWidth } from "../World/World";
 export const ForestFloor = () => {
 
     const leftCornerTexture = Texture.from('../public/assets/1 Tiles/Tile_01.png');
-    const tile02Texture = Texture.from('../public/assets/1 Tiles/Tile_02.png');
+    const middleTileTexture = Texture.from('../public/assets/1 Tiles/Tile_02.png');
     const rightCornerTexture = Texture.from('../public/assets/1 Tiles/Tile_03.png');
 
     return (
         <>
             <Sprite
                 texture={leftCornerTexture}
-                x = {leftCornerTexture.width}
-                y = {WindowHeight - leftCornerTexture.height}
+                x = {0}
+                y = {WindowHeight - (leftCornerTexture.height*2)}
                 scale={2}
             />
             
-            <Sprite
-                texture={tile02Texture}
-                x = {leftCornerTexture.width * 2}
-                y = {WindowHeight - leftCornerTexture.height}
-                scale={2}
-            />
+            {
+                Array(34).fill(0).map((_, i) => (
+                    <Sprite
+                        texture={middleTileTexture}
+                        x = {leftCornerTexture.width + i*(middleTileTexture.width)}
+                        y = {WindowHeight - (middleTileTexture.height*2)}
+                        scale={2}
+                    />
+                ))
+            }
             
             <Sprite
                 texture={rightCornerTexture}
-                x = {WindowWidth - rightCornerTexture.width}
-                y = {WindowHeight - rightCornerTexture.height}
+                x = {WindowWidth - (rightCornerTexture.width*2)}
+                y = {WindowHeight - (rightCornerTexture.height*2)}
                 scale={2}
             />
 
